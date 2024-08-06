c01fc0b26f43177d5a57977542fcb2f4a259a342
Xelby09890 <alexis.carreos19@gmail.com>
2024-08-05 23:46:30 -0600

    Week 3 commit1

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
+// //setting our view engine
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
+
+// app.get('/', (req, res) => {
+//     const data = {
+//         title: "Hello World",
+//         message: "Welcome to my website! This is the data for the object"
+//     }
+//     res.render('index', {data});
+// });
+
+// app.get('/users/:id', (req, res) => {
+//     const userId = req.params.id;
+//     res.send(`User with ID ${userId}`);
+// })
+
+// //Aug 05, 2024
+// const express = require('express');
+// const ejs = require('ejs');
+
+// //Linking the page route with app.js
+// const router = require('./routes/pages'); 
+
+// //Setting up EJS as the view engine
+// const app = express();
+// const PORT = 3000;
+
+// //implementing routes using middleware
+// app.use(router);
+
+// //Starting the server on the specified port.
+// app.listen(PORT, () => {
+//      console.log(`Server is running on http://localhost:$(PORT)`);
+// });
 
 const express = require('express');
-const ejs = require('ejs');
-
 const app = express();
+
 const PORT = 3000;
-
-//setting our view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
+//Query parameters
 app.get('/', (req, res) => {
-    const data = {
-        title: "Hello World",
-        message: "Welcome to my website! This is the data for the object"
-    }
-    res.render('index', {data});
+     const id = req.query.id;
+     const username = req.query.username;
+     //Assuming the user ID and username are required parameters.
+     res.send(`User ID: ${id}, Username: ${username}`);
 });
 
-app.get('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    res.send(`User with ID ${userId}`);
-})
 app.listen(PORT, () => {
-    console.log('Server started on port $(PORT)');
+     console.log(`Server is running on http://localhost:$(PORT)`);
 });
---
+++ routes/pages.js
@@ -0,0 +1,13 @@
+//routes/pages.js file
+const express = require('express');
+const router = express.Router();
+
+//callback function
+router.get('/', (req, res) => {
+    res.send("This is the homepage")
+});
+router.get("/about", (req, res) => {
+    res.send("This is the about us page")    
+});
+
+module.exports = router;
